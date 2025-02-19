81529f1cdb87aeb8c9135c592cde708613648990
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-02-19 14:08:18 +0100

    convert recursively and generate config

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,30 @@ struct Cli {
 
     #[arg(short, long, default_value_t = 1)]
     lossless: u8,
+
+    #[arg(short, long, default_value_t = 6)]
+    method: u8,
 }
 
 impl Cli {
+    fn input_path(&self) -> Result<PathBuf, Box<dyn std::error::Error>> {
+        if self.input.try_exists().is_err() {
+            Err(format!(
+                "The path: {} does not exist!",
+                &self.input.to_str().unwrap()
+            ))?
+        }
+
+        if !self.input.is_file() && !self.input.is_dir() {
+            Err(format!(
+                "The path: {} does not exist!",
+                self.input.to_str().unwrap()
+            ))?
+        }
+
+        Ok(self.input.clone())
+    }
+
     fn output_path(&self) -> Result<PathBuf, Box<dyn std::error::Error>> {
         let output_dir = match &self.output {
             Some(path) => {
@@ -53,64 +74,104 @@ impl Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let output_path = args
+    let output_path: PathBuf = args
         .output_path()
         .map_err(|_| "Output file is invalid")
         .unwrap();
 
-    if args.input.try_exists().is_err() {
-        return Err(format!(
-            "The path: {} does not exist!",
-            args.input.to_str().unwrap()
-        ))?;
-    }
+    let input_path: PathBuf = args.input_path().map_err(|error| error).unwrap();
 
-    if !args.input.is_file() && !args.input.is_dir() {
-        Err(format!(
-            "The path: {} does not exist!",
-            args.input.to_str().unwrap()
-        ))?
-    }
-
-    let mut config: WebPConfig = WebPConfig::new().unwrap();
-    config.lossless = args.lossless as i32;
-    config.quality = args.quality as f32;
-    config.method = 5;
-    // Multi threading
-    config.thread_level = 1;
+    let config = generate_config(&args);
 
     let now = Instant::now();
 
-    let _ = convert_file(args.input, output_path, config);
+    convert_recursively(&input_path, &output_path, &config, 0);
 
     let elapsed_time = now.elapsed();
-    println!(
-        "Running slow_function() took {} seconds.",
-        elapsed_time.as_millis()
-    );
+    println!("Running took {} seconds.", elapsed_time.as_millis());
 
     Ok(())
 }
 
+const MAX_LEVEL: u8 = 5;
+fn convert_recursively(
+    input_path: &PathBuf,
+    output_path: &PathBuf,
+    config: &WebPConfig,
+    level: u8,
+) {
+    if level >= MAX_LEVEL {
+        println!("Max level reached. Returning...");
+        return;
+    }
+
+    if input_path.is_dir() {
+        let input_dir = input_path.read_dir().map_err(|error| error).unwrap();
+        for path in input_dir {
+            if path.is_err() {
+                continue;
+            }
+
+            let path = path.unwrap().path();
+
+            if path.is_dir() {
+                let output_path_with_dir = &output_path.join(&path.file_name().unwrap());
+                println!("{:?}", output_path_with_dir);
+                convert_recursively(&path, &output_path_with_dir, config, level + 1);
+            } else {
+                let _ = convert_file(&path, &output_path, &config);
+            }
+        }
+    } else {
+        let _ = convert_file(&input_path, &output_path, &config);
+    }
+}
+
+fn generate_config(args: &Cli) -> WebPConfig {
+    let mut config: WebPConfig = WebPConfig::new().unwrap();
+    config.lossless = args.lossless as i32;
+    config.quality = args.quality as f32;
+    config.method = args.method as i32;
+    // Multi threading
+    config.thread_level = 1;
+
+    config
+}
+
 fn convert_file(
-    input: PathBuf,
-    output: PathBuf,
-    config: WebPConfig,
+    input: &PathBuf,
+    output: &PathBuf,
+    config: &WebPConfig,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let now = Instant::now();
+
     let file_name = &input
         .file_stem()
         .ok_or_else(|| format!("The file name: {:?} does not exist!", &input.file_name()))?
         .to_string_lossy()
         .to_string();
 
-    let img = open_image_from_path(input).unwrap();
+    let img = open_image_from_path(input.clone()).unwrap();
 
     let result = image_to_webp(img, &config);
     let webp = result.map_err(|_| "Failed to convert image")?;
 
     let output_path = output.join(file_name).with_extension("webp");
 
+    if !(&output_path).exists() {
+        fs::create_dir_all(&output_path.parent().unwrap())?;
+    }
+
     fs::write(&output_path, &*webp).unwrap();
+    let elapsed_time = now.elapsed();
+
+    println!(
+        "{:?} - Input: {:.1} KB -> Output: {:.1} KB. Duration: {:?} ms",
+        input.file_name().unwrap(),
+        input.metadata().unwrap().len() as f64 / 1024.0,
+        output_path.metadata().unwrap().len() as f64 / 1024.0,
+        elapsed_time.as_millis()
+    );
 
     Ok(())
 }
